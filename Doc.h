deb314d8e70511b801ce38ded1864099296c5fb9
on5cg <on5cg.christian@gmail.com>
2024-11-14 09:55:53 +0100

    modif Doc

--- Doc.h
+++ Doc.h
@@ -10,7 +10,22 @@ Synchronisation avec le dépôt :
 git reset --hard origin/main
 
 
+En général :
 
+1. Vérifier l’état du dépôt : 
+git status
+2. Stager les modifications : 
+git add .
+3. Valider les changements : 
+git commit -m "Message de validation"
+4. Récupérer les mises à jour du dépôt distant : 
+git fetch
+5. Fusionner les changements du dépôt distant : 
+git merge origin/<nom_de_branche>
+6. Mettre à jour la branche locale directement (remplace fetch et merge): 
+git pull
+7. Pousser les changements locaux : 
+git push
 
 
 
